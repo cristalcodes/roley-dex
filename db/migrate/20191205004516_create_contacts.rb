c96032479a15f78e8889b59f2f41151be6f2a47c
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-12-04 22:33:51 -0500

    created tables for all three migrations (create_users/contacts/notes)

--- db/migrate/20191205004516_create_contacts.rb
+++ db/migrate/20191205004516_create_contacts.rb
@@ -1,4 +1,13 @@
 class CreateContacts < ActiveRecord::Migration
   def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :mailing_address
+      t.string :phone
+      t.string :birthday
+      t.integer :user_id
+      t.integer :note_id
+    end
   end
 end
--- db/migrate/20191205024712_create_notes.rb
+++ db/migrate/20191205024712_create_notes.rb
@@ -1,4 +1,9 @@
 class CreateNotes < ActiveRecord::Migration
   def change
+    create_table :notes do |t|
+      t.string :date
+      t.text :content
+      t.integer :contact_id
+    end
   end
 end
