197b806ee598ca69ac7d187ec271c5e60fc05b17
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-12-05 15:40:43 -0500

    Wrote post contacts/new route method and get contacts/:id route method. Wrote ‘/contacts/show’ erb.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,11 +19,31 @@ class ContactsController < ApplicationController
   end
 
   post "/contacts" do
-    params.to_s
+    if logged_in?
+      #
+      if params[:first_name]=="" || params[:phone]==""
+        redirect to "/contacts/new"
+      else
+        @contact= current_user.contacts.build(params)
+        if @contact.save
+          redirect to "/contacts/#{@contact.id}"
+        else
+          redirect to "/contacts/new"
+        end
+      end
+      #
+    else
+      redirect to "/"
+    end
   end
 
   get "/contacts/:id" do
-
+    if logged_in?
+      @contact= Contact.find_by_id(params[:id])
+      erb :'contacts/show'
+    else
+      redirect to '/'
+    end
   end
 
   get "/contacts/:id/edit" do
