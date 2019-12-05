2bdb39949b2deb6b08794498a4498ce925f4dcae
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-12-05 18:08:50 -0500

    53. Added log out button to the layout (so that it sits in the footer and only appears if not currently logged in), and a login/logout method in the User controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    params.to_s
     if params[:first_name]=="" || params[:last_name]=="" || params[:email]=="" || params[:password]==""
       redirect to '/signup'
     else
@@ -17,6 +16,25 @@ class UsersController < ApplicationController
       @user.save
       session[:user_id]=@user.id
       redirect to '/contacts'
+    end
+  end
+
+  post '/login' do
+    user=User.find_by(email:params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id]=user.id
+      redirect to "/contacts"
+    else
+      redirect to "/"
+    end
+  end
+
+  get "/logout" do
+    if logged_in?
+      session.clear
+      redirect to "/"
+    else
+      redirect to "/"
     end
   end
 
