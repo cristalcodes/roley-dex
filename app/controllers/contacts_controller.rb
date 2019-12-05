7b059c48d6e12b46d94775386a6e46f2ea744f84
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-12-05 14:49:48 -0500

    35. Set up contacts index page (get ‘/contacts’), and in ContactsController Set Up all routes to fill in.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,37 @@
 class ContactsController < ApplicationController
 
+  get "/contacts" do
+    if logged_in?
+      @user= current_user
+      @contacts= @user.contacts
+      erb :'contacts/index'
+    else
+      redirect to '/'
+    end
+  end
+
+  get "/contacts/new" do
+  end
+
+  post "/contacts" do
+  end
+
+  get "/contacts/:id" do
+
+  end
+
+  get "/contacts/:id/edit" do
+
+  end
+
+  patch "/contacts/:id" do
+
+  end
+
+  delete "/contacts/:id/delete" do
+
+  end
+
+
+
 end
