a93cc2f41bb357e675ed77cac0ac0546d390f046
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-12-11 16:56:15 -0500

    no changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user=User.find_by(email:params[:email])
+    user= User.find_by(email:params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id]=user.id
       redirect to "/contacts"
